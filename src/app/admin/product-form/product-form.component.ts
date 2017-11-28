a2223770ae07d0446338cdd11a2d6dc328d94a6c
Ashley MacWhirter <macwhirtera@gmail.com>
2017-11-27 22:06:25 -0500

    added card preview of new product. added manage product table view

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from "../../category.service";
 import {ProductService} from "../../product.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-form',
@@ -11,12 +12,16 @@ import {ProductService} from "../../product.service";
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
   }
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
   ngOnInit() {
