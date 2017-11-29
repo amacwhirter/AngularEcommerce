f8fe173a2b073c6e5732b172f08aaeb4d8c5e875
Ashley MacWhirter <macwhirtera@gmail.com>
2017-11-28 21:40:41 -0500

    more refactoring, cards showing and filtering properly, need more style

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -8,7 +8,7 @@ import {Product} from "../models/product";
 })
 export class ProductCardComponent {
   @Input('product') product: Product;
-  @Input('show-actions') showActions: true;
+  @Input('show-actions') showActions = true;
 
   constructor() { }
 
