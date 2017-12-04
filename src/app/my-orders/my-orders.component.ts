4a24eb596171cd8fd320817b7583599b90ab2287
Ashley MacWhirter <macwhirtera@gmail.com>
2017-12-04 11:51:25 -0500

    added order views

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {AuthService} from "../auth.service";
+import {OrderService} from "../order.service";
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent {
+  orders$;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService) {
 
-  ngOnInit() {
+    this.orders$ = authService.user$.switchMap(u => orderService.getOrdersByUser(u.uid));
   }
-
 }
