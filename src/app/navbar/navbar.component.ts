f25134ffcaff31091475e60f4766828ecdc90727
Ashley MacWhirter <macwhirtera@gmail.com>
2017-12-03 19:40:57 -0500

    refactoring code

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../auth.service";
 import {AppUser} from "../models/app-user";
+import {ShoppingCartService} from "../shopping-cart.service";
+import {Observable} from "rxjs/Observable";
+import {ShoppingCart} from "../models/shopping-cart";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   appUser: AppUser;
+  cart$: Observable<ShoppingCart>;
 
-  constructor(private auth: AuthService) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser)
+  constructor(private auth: AuthService,
+              private shoppingCartService: ShoppingCartService) {
   }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    this.cart$ = await this.shoppingCartService.getCart();
+    };
+
 
   logout() {
     this.auth.logout();
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,7 +1,10 @@
 import {Injectable} from '@angular/core';
-import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFireDatabase, FirebaseObjectObservable} from "angularfire2/database";
 import {Product} from "./models/product";
+import {ShoppingCart} from "./models/shopping-cart";
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class ShoppingCartService {
@@ -15,9 +18,10 @@ export class ShoppingCartService {
     });
   }
 
-  async getCart() {
+  async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
-    return this.db.object('/shopping-carts/' + cartId);
+    return this.db.object('/shopping-carts/' + cartId)
+      .map(x => new ShoppingCart(x.items));
   }
 
   private getItem(cartId: string, productId: string) {
