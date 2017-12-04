4bd6a3b65e576c08a8afa82e5686a5db0fddfd8c
Ashley MacWhirter <macwhirtera@gmail.com>
2017-12-03 19:10:16 -0500

    quanities now reflected on the footer of the product card. can add or delete number of items in footer. new number automatically updates in firebase db async

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,21 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from "../product.service";
 import {ActivatedRoute} from "@angular/router";
 import {Product} from "../models/product";
 import 'rxjs/add/operator/switchMap';
+import {ShoppingCartService} from "../shopping-cart.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(route: ActivatedRoute,
-              productService: ProductService) {
+              productService: ProductService,
+              private shoppingCartService: ShoppingCartService) {
+
     productService
       .getAll()
       .switchMap(products => {
@@ -31,5 +37,14 @@ export class ProductsComponent {
           this.products;
       });
 
+  }
+
+  async ngOnInit() {
+    this.subscription = (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -15,7 +15,8 @@ export class ShoppingCartService {
     });
   }
 
-  private getCart(cartId: string) {
+  async getCart() {
+    let cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId);
   }
 
@@ -23,7 +24,7 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId(): Promise<string> {
     let cartId = localStorage.getItem('cartId');
     if (cartId) return cartId;
 
@@ -33,10 +34,18 @@ export class ShoppingCartService {
   }
 
   async addToCart(product: Product) {
-    let cartId = await this.getOrCreateCart();
+    this.updateItemQuantity(product, 1);
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItemQuantity(product, -1);
+  }
+
+  private async updateItemQuantity(product: Product, change: number){
+    let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
     item$.take(1).subscribe(item => {
-      item$.update({ product: product, quantity: (item.quantity || 0) + 1 });
+      item$.update({ product: product, quantity: (item.quantity || 0) + change });
     });
   }
 }
